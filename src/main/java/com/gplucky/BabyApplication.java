60fbcacc8d1846e6e095e011b179b10f1504b56f
Kevin <gubaijing2004@163.com>
2017-01-21 23:13:08 +0800

    inti

---
+++ src/main/java/com/gplucky/BabyApplication.java
@@ -0,0 +1,12 @@
+package com.gplucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyApplication.class, args);
+	}
+}
